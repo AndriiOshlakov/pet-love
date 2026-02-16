f7d5b8bbd30facee6615099ad8cd0e9f9e151507
Andrii <andreyoshlakov18051986@gmail.com>
2026-02-16 13:21:45 +0200

    comment friends page

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,18 +1,18 @@
-import Container from '@/components/Container/Container';
-import Title from '@/components/Title/Title';
-import css from './Friends.module.css';
-import FriendsList from '@/components/FriendsList/FriendsList';
-import { fetchFriends } from '@/lib/api/serverApi';
+// import Container from '@/components/Container/Container';
+// import Title from '@/components/Title/Title';
+// import css from './Friends.module.css';
+// import FriendsList from '@/components/FriendsList/FriendsList';
+// import { fetchFriends } from '@/lib/api/serverApi';
 
-export default async function Friends() {
-  const ourFriends = await fetchFriends();
+// export default async function Friends() {
+//   const ourFriends = await fetchFriends();
 
-  return (
-    <Container>
-      <section className={css.wrapper}>
-        <Title title="Our friends" />
-        <FriendsList list={ourFriends} />
-      </section>
-    </Container>
-  );
-}
+//   return (
+//     <Container>
+//       <section className={css.wrapper}>
+//         <Title title="Our friends" />
+//         <FriendsList list={ourFriends} />
+//       </section>
+//     </Container>
+//   );
+// }
